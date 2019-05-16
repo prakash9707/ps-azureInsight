73812b78346be467c6eda80093cbcf9385eaff49
prakash9707 <prakashpp666666@gmail.com>
2019-05-16 10:34:49 +0530

    found

--- server.js
+++ server.js
@@ -39,10 +39,8 @@ const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
 const adapter = new BotFrameworkAdapter({
     appId: endpointConfig.appId || process.env.microsoftAppID,
     appPassword: endpointConfig.appId || process.env.MicrosoftAppPassword,
-    // channelService: process.env.ChannelService,
-    // openIdMetadata: process.env.BotOpenIdMetadata
-    // appId : '',
-    // appPassword : '',
+    channelService: process.env.ChannelService,
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 
 
